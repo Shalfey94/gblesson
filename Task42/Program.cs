b5ef88182a19ab584e7c0e0a570d59ac6eb5981f
SergeyKharkovskiy <shalfey.90@mail.ru>
2023-01-19 17:18:17 +0300

    Задача 42

---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = BinaryNumber(number);
+Console.WriteLine(result);
+
+int BinaryNumber(int num)
+{
+    int count = 1;
+    int binNum = 0;
+    while(num > 0)
+    {
+        binNum = binNum + num % 2 * count;
+        num = num / 2;
+        count = count * 10;
+    }
+    return binNum;
+}
