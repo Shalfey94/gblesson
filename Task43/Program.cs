ade8d4f3e111fa05b85567afff6bff0424b9dfd4
SergeyKharkovskiy <shalfey.90@mail.ru>
2023-01-20 15:51:38 +0300

    Задача 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -4,18 +4,18 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-int k1 = EnterNumber("Введите k1:");
-int b1 = EnterNumber("Введите b1:");
-int k2 = EnterNumber("Введите k2:");
-int b2 = EnterNumber("Введите b2:");
+double k1 = EnterNumber("Введите k1:");
+double b1 = EnterNumber("Введите b1:");
+double k2 = EnterNumber("Введите k2:");
+double b2 = EnterNumber("Введите b2:");
 
 var tuple = IntersectionPoint(k1, b1, k2, b2);
 Console.WriteLine(tuple);
 
-(double, double) IntersectionPoint(int numK1, int numB1, int numK2, int numB2)
+(double, double) IntersectionPoint(double numK1, double numB1, double numK2, double numB2)
 {
     double xPoint = (numB2 - numB1) / (numK1 - numK2);
-    double yPoint = numK1 * xPoint - numB1;
+    double yPoint = numK1 * xPoint + numB1;
     var res = (xPoint, yPoint);
     return res;
 }
