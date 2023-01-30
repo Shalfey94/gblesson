c2efe0cce6f46a07a28d480735012fee433ce3e2
SergeyKharkovskiy <shalfey.90@mail.ru>
2023-01-30 11:44:02 +0300

    задача 67

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigitNumber(number);
+Console.WriteLine(result);
+
+int SumDigitNumber(int n)
+{
+    if(n == 0) return 0;
+    return n % 10 + SumDigitNumber(n/10);
+}
+
+
