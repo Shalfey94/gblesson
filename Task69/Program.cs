56b73c52bd2530f44666ffd42082055ecd97af26
SergeyKharkovskiy <shalfey.90@mail.ru>
2023-01-30 11:50:20 +0300

    задача 69

---
+++ Task69/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+Console.WriteLine("Введите натуральное число:");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число:");
+int numN = Convert.ToInt32(Console.ReadLine());
+int result = DegreeNumber(numM, numN);
+Console.WriteLine(result);
+
+int DegreeNumber(int n, int m)
+{
+    if(m == 0) return 1;
+    return n *  DegreeNumber(n, m - 1);
+}
